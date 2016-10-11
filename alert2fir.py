879ae303793d48f859c1380cc55ef638c844870f
weslambert <wlambertts@gmail.com>
2016-10-10 20:57:33 -0400

    Update alert2fir.py

--- alert2fir.py
+++ alert2fir.py
@@ -1,61 +1,64 @@
 #!/usr/bin/python
-import sys, struct, json, urllib2, base64, requests, os, re, logging, socket, MySQLdb
+import base64, json, logging, MySQLdb, os, re, requests, socket, struct, sys, urllib2
 from requests.auth import HTTPBasicAuth
 
 logging.basicConfig(filename='/var/log/fir_incident.log',level=logging.DEBUG)
 
-############################
-## Get SRC\DST IP
-#############################
+sig_list = []
 
-ip_array = []
-
-db = MySQLdb.connect(host="localhost",
+db = MySQLdb.connect (host="localhost",
                      user="sguil",
                      passwd="password",
                      db="securityonion_db")
-
 cur = db.cursor()
 
-# Get all events (within the last minute) from securityonion_db where uncategorized and priority of "1"
-cur.execute("select * FROM event where status = '0' and priority = '1' and event.timestamp<curdate() and event.timestamp>DATE_ADD(CURDATE(), INTERVAL -1 MINUTE);")
+cur.execute("select distinct signature from event where status = '0' and priority = '1' and event.signature_gen != 10001 and event.signature_id != 420042 and event.timestamp>date_sub(now(), INTERVAL 1 MINUTE);")
 
-# Get alert values
+db.close()
+
 for row in cur.fetchall():
-    sig_msg = row[2]
-    date = row[6]
-    src_ip = socket.inet_ntoa(struct.pack('!L',row[13]))
-    dst_ip = socket.inet_ntoa(struct.pack('!L',row[14]))
-    db.close()
-    ip_array.append(src_ip)
-    ip_array.append(dst_ip)
-    logging.info('Source IP address set to ' + src_ip + '.')
-    logging.info('Destination IP address set to ' + dst_ip + '.')
+    signature = row[0]
+    sig_list.append(signature)
+    
+for i in sig_list:
+ db = MySQLdb.connect (host="localhost",
+                      user="sguil",
+                      passwd="password",
+                      db="securityonion_db")
 
+ cur = db.cursor()
+ cur.execute("select timestamp from event where signature = '%s' and status = '0' and priority = '1' and event.signature_gen != 10001 and event.signature_id != 420042 and event.timestamp>date_sub(now(), INTERVAL 1 MINUTE) order by timestamp limit 1;" %(i))
+ for row in cur.fetchall():
+    date = row [0]
+ cur.execute("select distinct src_ip,dst_ip from event where signature = '%s' and status = '0' and priority = '1' and event.signature_gen != 10001 and event.signature_id != 420042 and event.timestamp>date_sub(now(), INTERVAL 1 MINUTE);" %(i))
+ for row in cur.fetchall():
+    body_data = []
+    src_ip = socket.inet_ntoa(struct.pack('!L',row[0]))
+    dst_ip = socket.inet_ntoa(struct.pack('!L',row[1]))
+    body_data.append(src_ip)
+    body_data.append(dst_ip)
+ db.close()
+ fir = "https://local-fir-server.com/api/incidents"
 
-fir = "https://fir-server.com/api/incidents"
-
-headers = {
-   'Authorization' : '<individual_user_token>',
+ headers = {
+   'Authorization' : 'Token <token>',
    'Content-type' : 'application/json'
-}
+ }
 
-#response = requests.get(fir, headers=headers, verify=False)
-#print response.content
+ response = requests.get(fir, headers=headers, verify=False)
+ #print response.content
 
-data = {
+ data = {
 
     "actor" : "3",
     "category": "3",
     "confidentiality": "1",
-    "description": "This is a test ticket created by the API. SRCIP: " + src_ip ,
+    "description": body_data,
     "detection": "2",
     "plan" : "8",
     "severity": "2",
-    "subject": sig_msg
-}
-
-response = requests.post(fir,
-         headers=headers, data=json.dumps(data), verify=False)
-print response.content
-logging.warning(response.content)
+    "subject": i
+ }
+ response = requests.post(fir, headers=headers, data=json.dumps(data), verify=False)
+ #print response.content
+ #logging.warning(response.content)
